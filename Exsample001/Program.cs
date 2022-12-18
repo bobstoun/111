cc1249c54acf6f6fce6bf8418b2474ff9e41fd61
bobstoun <119942184+bobstoun@users.noreply.github.com>
2022-12-18 16:06:20 +0300

    add string cod

--- Exsample001/Program.cs
+++ Exsample001/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int z1 = 56;
+int x2 = 93;
+int c3 = 95;
+int v4 = 25;
+int h5 = 64;
+int g6 = 41;
+int f7 = 42;
+int e8 = 42;
+int d9 = 43;
+
+//int max = 25;
+//if (z1 > max) max = z1;
+//if (d9 > max) max = d9;
+//if (e8 > max) max = e8;
+//if (f7 > max) max = f7;
+//if (g6 > max) max = g6;
+//if (h5 > max) max = h5;
+//if (v4 > max) max = v4;
+//if (c3 > max) max = c3;
+//if (x2 > max) max = x2;
+
+Console.WriteLine(max);
